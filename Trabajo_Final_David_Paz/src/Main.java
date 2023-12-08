8bd74feaf9e22a06825288ca089e5be0d3e12d34
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-12-08 20:19:22 -0300

    feat: add new Team

--- Trabajo_Final_David_Paz/src/Main.java
+++ Trabajo_Final_David_Paz/src/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
 
+        Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
+
         Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
 
         messi.getTeam().getPlayers().add(messi);
+        dimaria.getTeam().getPlayers().add(dimaria);
 
         System.out.println(messi.getTeam().getName());
     }
