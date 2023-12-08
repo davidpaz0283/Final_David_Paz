94456d204c2e877ac3f6f21ea8f25221f44703ab
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-12-08 19:37:55 -0300

    feat: add team class

--- Trabajo_Final_David_Paz/src/estadisticas/Team.java
+++ Trabajo_Final_David_Paz/src/estadisticas/Team.java
@@ -18,4 +18,18 @@ public class Team {
     }
     public void removePlayer (Player player){
     }
+
+
+    public int getTableScore() {
+        return tableScore;
+    }
+    public void setTableScore(int tableScore) {
+        this.tableScore = tableScore;
+    }
+    public int getGoalCounter() {
+        return goalCounter;
+    }
+    public void setGoalCounter(int goalCounter) {
+        this.goalCounter = goalCounter;
+    }
 }
