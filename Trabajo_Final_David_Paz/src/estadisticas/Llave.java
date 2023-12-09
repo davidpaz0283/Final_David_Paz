dd2b1d97db265a6b76487779173fd85d015f4129
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-12-08 22:13:48 -0300

    feat: add llave class

---
+++ Trabajo_Final_David_Paz/src/estadisticas/Llave.java
@@ -0,0 +1,21 @@
+package estadisticas;
+
+import java.util.ArrayList;
+public class Llave extends Season{
+    public ArrayList<Team> getNextStepTeams() {
+        ArrayList<Team> winners = new ArrayList<Team>();
+        ArrayList<Match> matches = getMatches();
+        for (int i = 0; i < matches.size(); i++) {
+            Match actualMatch = matches.get(i);
+            if(actualMatch.isVisitantWinner()) {
+                winners.add(actualMatch.getVisitantTeam());
+            } else if(actualMatch.isLocalWinner()) {
+                winners.add(actualMatch.getLocalTeam());
+            } else {
+                
+                winners.add(actualMatch.getVisitantTeam());
+            }
+        }
+        return winners;
+    }
+}
